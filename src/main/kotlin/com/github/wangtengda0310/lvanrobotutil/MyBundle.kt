42a22a661e3529924c9ce1d5e57a02d74e482286
GitHub Action <action@github.com>
2024-09-25 08:03:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangtengda0310/lvanrobotutil/MyBundle.kt
+++ src/main/kotlin/com/github/wangtengda0310/lvanrobotutil/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wangtengda0310.lvanrobotutil
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/wangtengda0310/lvanrobotutil/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/wangtengda0310/lvanrobotutil/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wangtengda0310.lvanrobotutil.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
